fd147cc8ab1ed78785cbc444f61927df476ba8ce
suriarasai <suria.r.asai@gmail.com>
2017-06-12 20:19:10 +0800

    laps in spring boot

---
+++ LapsBoot/src/main/webapp/WEB-INF/decorators/Header.jsp
@@ -0,0 +1,10 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<div>
+	<h1 align="right">
+		YOUR HEADER
+	</h1>
+
+</div>
+
+
---
+++ LapsBoot/src/test/java/edu/iss/laps/LapsBootApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.iss.laps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LapsBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
